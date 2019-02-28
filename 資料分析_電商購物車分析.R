8398b2832db696fe5ac9a9109b754b521999945d
Dennis <jayshine727@gmail.com>
2019-02-28 17:59:08 +0800

    To integrate

---
+++ 資料分析_電商購物車分析.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+library(knitr)
+salesTable <- read_csv("/Users/Dennis/data_science/practice_data/SalesTable.csv")
+clientTable <- read_csv("/Users/Dennis/data_science/practice_data/ClientTable.csv")
+productTable <- read_csv("/Users/Dennis/data_science/practice_data/ProductTable.csv")
